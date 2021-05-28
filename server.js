03ae03c6eba8a5be470e04dc44733e07e05cfb98
satadeep3927 <satadeep3927@users.noreply.github.com>
2021-05-29 00:12:10 +0530

    version 1 youtube downloader created

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+app.use(cors());
+app.listen(4000, () => {
+    console.log('Server Works !!! At port 4000');
+});
+app.get('/download', (req,res) => {
+var URL = req.query.url;
+var format = req.query.format
+if(format==="mp3"){
+    res.header('Content-Disposition', 'attachment; filename="video.mp3"');
+    ytdl(URL, {
+        format: 'mp3'
+        }).pipe(res);
+}
+else if(format==="mp4"){
+    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+    ytdl(URL, {
+        format: 'mp4'
+        }).pipe(res);
+}
+});
